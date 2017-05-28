c8bbe27b1f2226e894b70b83568a5770b49cded1
dagilleland <dagilleland@shaw.ca>
2017-05-28 15:42:51 -0600

    Configure to use IdentityManager
    Again as with Identity Server, we’ll need to enable RAMMFAR in your
    web.config when hosting in IIS, in order for Identity Manager’s embedded
    resources to be loaded correctly (otherwise you’ll most likely be met
    with a blank screen).

--- ASP Identity/DemoIdentity/BLL/ApplicationUserManager.cs
+++ ASP Identity/DemoIdentity/BLL/ApplicationUserManager.cs
@@ -11,7 +11,7 @@ namespace DemoIdentity.BLL
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
-        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+        public ApplicationUserManager(ApplicationUserStore store)
             : base(store)
         {
         }
--- ASP Identity/Website/Startup.cs
+++ ASP Identity/Website/Startup.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.Owin;
+﻿using DemoIdentity.BLL;
+using DemoIdentity.DAL;
+using DemoIdentity.Entities;
+using IdentityManager;
+using IdentityManager.AspNetIdentity;
+using IdentityManager.Configuration;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(Website.Startup))]
@@ -7,6 +14,32 @@ namespace Website
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
             ConfigureAuth(app);
+
+            app.Map("/idm", idm =>
+            {
+                var factory = new IdentityManagerServiceFactory();
+                factory.IdentityManagerService = new Registration<IIdentityManagerService, ApplicationIdentityManagerService>();
+                factory.Register(new Registration<ApplicationUserManager>());
+                factory.Register(new Registration<ApplicationUserStore>());
+                factory.Register(new Registration<ApplicationRoleManager>());
+                factory.Register(new Registration<ApplicationRoleStore>());
+                //factory.Register(new Registration<ApplicationDbContext>(resolver => new ApplicationDbContext("foo")));
+                factory.Register(new Registration<ApplicationDbContext>());
+
+                idm.UseIdentityManager(new IdentityManagerOptions
+                {
+                    Factory = factory
+                });
+            });
+        }
+    }
+
+    public class ApplicationIdentityManagerService :
+        AspNetIdentityManagerService<ApplicationUser, string, IdentityRole, string>
+    {
+        public ApplicationIdentityManagerService(ApplicationUserManager userMgr, ApplicationRoleManager roleMgr)
+            : base(userMgr, roleMgr)
+        {
         }
     }
 }
