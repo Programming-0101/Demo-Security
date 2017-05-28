dd399ea5a7957b0d0ddde6c0cf27f9b34d63d1d6
dagilleland <dagilleland@shaw.ca>
2017-05-28 14:15:35 -0600

    Copy Identity Classes to Class Library

---
+++ ASP Identity/DemoIdentity/BLL/ApplicationSignInManager.cs
@@ -0,0 +1,26 @@
+﻿using DemoIdentity.Entities;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DemoIdentity.BLL
+{
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+    {
+        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager) :
+            base(userManager, authenticationManager)
+        { }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((ApplicationUserManager)this.UserManager);
+        }
+
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
+        }
+    }
+}
---
+++ ASP Identity/DemoIdentity/DAL/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DemoIdentity.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DemoIdentity.DAL
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
