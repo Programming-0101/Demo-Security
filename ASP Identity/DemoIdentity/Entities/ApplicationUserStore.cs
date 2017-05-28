4f452fb0db3eda28bb4c928dcef10344aefbc867
dagilleland <dagilleland@shaw.ca>
2017-05-28 15:13:27 -0600

    Wrap more Identity classes
    It can be useful to wrap the User and Role stores and the Role Manager,
    just as the starter template had wrapped the IdentityUser and
    UserManager.
    
    This will also be helpful for introducing IdentityManager as a quick
    user/role management utility in the web application.

---
+++ ASP Identity/DemoIdentity/Entities/ApplicationUserStore.cs
@@ -0,0 +1,18 @@
+﻿using DemoIdentity.DAL;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoIdentity.Entities
+{
+    public class ApplicationUserStore : UserStore<ApplicationUser>
+    {
+        public ApplicationUserStore(ApplicationDbContext ctx)
+            : base(ctx)
+        {
+        }
+    }
+}
