a6a41bad44b74aa96a09d60531a9cf5ef5d83fb3
dagilleland <dagilleland@shaw.ca>
2017-05-28 23:38:30 -0600

    Add ApplicationUser profile information

--- ASP Identity/DemoIdentity/Entities/ApplicationUser.cs
+++ ASP Identity/DemoIdentity/Entities/ApplicationUser.cs
@@ -9,6 +9,10 @@ namespace DemoIdentity.Entities
     // You can add User data for the user by adding more properties to your User class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string OfficeLocation { get; set; }
+        public string PhoneExtension { get; set; }
+        public string JobTitle { get; set; }
+
         public ClaimsIdentity GenerateUserIdentity(ApplicationUserManager manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
